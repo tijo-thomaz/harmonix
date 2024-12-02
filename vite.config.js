cab630d9491dd81b0e7b5c38c3690684ddb88a27
Tijo Thomas <tijo1293@gmail.com>
2024-12-02 23:22:58 +0000

    fix updated the vite builder

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "./templates",
+  root: "./templates", // Your root folder for templates
+  base: "/harmonix/", // Set the base path for GitHub Pages
   build: {
-    outDir: "../docs", // Output for GH Pages
+    outDir: "../docs", // Output to the docs/ folder
     emptyOutDir: true,
   },
   server: {
